1d2dc0d831dcb9f76e84adce39435edd5ea01a65
alexscotttonge <alex@scotttonge.com>
2018-05-09 13:56:40 +0100

    basic styling for homepage

--- spec/features/visitor_views_homepage_spec.rb
+++ spec/features/visitor_views_homepage_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "viewing homepage" do
   scenario "to sign up to the blog" do
     visit root_path
 
-    expect(page).to have_content("Blog")
+    expect(page).to have_title("Blog")
   end
 end
