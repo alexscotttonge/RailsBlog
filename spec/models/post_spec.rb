cd25e037645be978d5f47cf14c79272939b61a80
alexscotttonge <alex@scotttonge.com>
2018-05-11 11:47:03 +0100

    Post model validates presence of title

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it "post title should be present" do
+    user = create :user
+    post = user.posts.create(title: "")
+
+    expect(post).to_not be_valid
+  end
+
   it "updates Post model" do
     user = create :user
-    user.posts.create
+    user.posts.create(title: "My little pony")
 
     expect(Post.count).to eq 1
   end
 
   it "user id should be present" do
     user = create :user
-    post = user.posts.create
+    post = user.posts.create(title: "My little pony")
 
     expect(user.id).to eq post.id
   end
