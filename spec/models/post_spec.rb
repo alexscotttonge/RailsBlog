c8a9d9a3dbe8f33582f770c8384d60e0967ed28e
alexscotttonge <alex@scotttonge.com>
2018-05-11 12:27:50 +0100

    Post model verifies content must be present

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,16 +8,23 @@ RSpec.describe Post, type: :model do
     expect(post).to_not be_valid
   end
 
+  it "post content should be present" do
+    user = create :user
+    post = user.posts.create(title: "My little pony", content: "")
+
+    expect(post).to_not be_valid
+  end
+
   it "updates Post model" do
     user = create :user
-    user.posts.create(title: "My little pony")
+    user.posts.create(title: "My little pony", content: "Lovely mane")
 
     expect(Post.count).to eq 1
   end
 
   it "user id should be present" do
     user = create :user
-    post = user.posts.create(title: "My little pony")
+    post = user.posts.create(title: "My little pony", content: "Lovely mane")
 
     expect(user.id).to eq post.id
   end
