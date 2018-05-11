a176287ec396196ee6cf8950968b088751934c98
alexscotttonge <alex@scotttonge.com>
2018-05-11 14:21:09 +0100

    updated User model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,22 @@
 require 'rails_helper'
 
+RSpec.describe User, "assocations" do
+  it { is_expected.to have_many :posts }
+end
+
+RSpec.describe User, "validations" do
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_presence_of :name }
+end
+
+RSpec.describe User, "secure password" do
+  it { is_expected.to have_secure_password }
+end
+
 RSpec.describe User, type: :model do
-
-  it "user email can't be blank" do
-    user = User.new(email: "")
-
-    expect(user).to_not be_valid
-  end
-
-  it "user name can't be blank" do
-    user = User.new(email: "email@example.com", name: "")
-
-    expect(user).to_not be_valid
-  end
-
-  it "has a secure password" do
-    user = User.new(email: "email@example.com", name: "name")
-
-    expect(user).to_not be_valid
-  end
-
   it "user is saved correctly to the database" do
     create :user
 
     expect(User.count).to eq 1
   end
-end
-
-RSpec.describe User, "assocations" do
-  it { is_expected.to have_many :posts }
 end
