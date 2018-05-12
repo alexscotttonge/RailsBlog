80711f7a54baf263d8966f3ef38841cfb2e86388
Alex <alex@scotttonge.com>
2018-05-12 16:29:50 +0100

    created index view for all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -21,8 +26,15 @@ class UsersController < ApplicationController
 
  private
 
-   def user_params
-     params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-   end
+    def user_params
+       params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root   "home_page#home"
-  get    '/signup',    to: 'users#new'
-  post   '/signup',    to: 'users#create'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
+  resources :posts,  only: [:create, :destroy]
 end
