65eaa30069e6dadd749f5ed87576f3e5c233e68b
alexscotttonge <alex@scotttonge.com>
2018-05-15 11:06:20 +0100

    added index view for all blog posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy, :edit]
   before_action :correct_user,   only: [:destroy, :edit]
 
+  def index
+    @posts = Post.all
+  end
+
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
