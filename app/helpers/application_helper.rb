d0524569ff80cb6acf9d8701b37afc8a18223bbc
alexscotttonge <alex@scotttonge.com>
2018-05-17 12:53:07 +0100

    Added markdown for blog
    Used Recarpet gem to render markdown on blogpost form preview and show page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  
+  def markdown(text)
+    redcarpet(redcarpet_renderer).render(text).html_safe
+  end
+
+  def redcarpet(renderer)
+    Redcarpet::Markdown.new(renderer)
+  end
+
+  def redcarpet_renderer
+    options = {
+      hard_wrap: true,
+      autolink: true
+    }
+    Redcarpet::Render::HTML.new(options)
+  end
 end
