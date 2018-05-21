b61c5ce85ed92fe1326b625d4e5baf45ced827e9
alexscotttonge <alex@scotttonge.com>
2018-05-21 14:43:39 +0100

    Signed in user can visit dashboard from homepage

---
+++ spec/features/user_visits_dashboard_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+include ManagingUser
+
+RSpec.feature "visiting the dashboard" do
+  context "when the user is logged in" do
+    scenario "when the user is on the homepage" do
+      log_user_in
+
+      visit root_path
+
+      expect(page).to have_css "h1", text: ("All blog posts")
+
+      within "header" do
+        click_on "Visit dashboard"
+      end
+
+      expect(page).to have_css "h1", text: "The dude"
+    end
+  end
+end
