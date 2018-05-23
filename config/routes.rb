c05ccaf71400a5e154d2725d5bd4ebfa0dfd93bf
alexscotttonge <alex@scotttonge.com>
2018-05-23 12:13:07 +0100

    deleted dashboard resource

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resource  :dashboard
   resources :users
   resources :posts
 end
