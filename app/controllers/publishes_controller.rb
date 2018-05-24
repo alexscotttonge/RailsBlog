a23efc8a48ec986d4d30dbeb9935ee4fb2e10f11
alexscotttonge <alex@scotttonge.com>
2018-05-24 11:05:28 +0100

    changed publishes#create action to use fetch instead of find

--- app/controllers/publishes_controller.rb
+++ app/controllers/publishes_controller.rb
@@ -1,7 +1,9 @@
 class PublishesController < ApplicationController
 
   def create
-    Post.find(params[:post_id]).update(published: true)
+    post_id = params.fetch(:post_id)
+    Post.find(post_id).update(published: true)
+    # prefer? Post.find(params[:post_id]).update(published: true)
     redirect_to root_path
   end
 
