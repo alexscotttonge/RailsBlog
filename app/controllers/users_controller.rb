11a1883ccd255c5c688cf53d53d298e4c640be31
alexscotttonge <alex@scotttonge.com>
2018-05-24 14:58:40 +0100

    Refactored User controller and views
    User controller now uses local variables instead of instance variables, corresponding views are also updated.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,28 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
 
   def index
-    @users = User.all
+    render locals: { users: User.all }
   end
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts
+    user = User.find(params[:id])
+    render locals: { user: user,
+                     posts: user.posts
+                   }
   end
 
   def new
-    @user = User.new
+    render locals: { user: User.new }
   end
 
   def create
-   @user = User.new(user_params)
-   if @user.save
-     flash[:success] = "Welcome to the blog"
-     redirect_to @user
-   else
-     render 'new'
-   end
+    user = User.new(user_params)
+    if user.save
+      flash[:success] = "Welcome to the blog"
+      redirect_to user
+    else
+      render :new, locals: { user: user }
+    end
  end
 
  private
