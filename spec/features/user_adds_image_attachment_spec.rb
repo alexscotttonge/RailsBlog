91497968f69f02de3b9cedb62712bb3e46a4d146
alexscotttonge <alex@scotttonge.com>
2018-05-24 12:06:43 +0100

    Restructured spec feature tests for attachment

--- spec/features/user_adds_image_attachment_spec.rb
+++ spec/features/user_adds_image_attachment_spec.rb
@@ -2,9 +2,21 @@ require "rails_helper"
 include ManagingUser
 
 RSpec.feature "adding an image to a blog post" do
-  context "when the user is logged in" do
-    xit "successfully adds an attachment" do
-      # need to decide how best to feature test uploading an attachment
+  context "when there is an attachment" do
+    it "the attachment is available on the posts#show view" do
+      log_user_in
+
+      fill_in "post_title", with: "xColor rocks"
+      fill_in "post_content", with: "Here are some reasons"
+      page.attach_file("post_uploads", Rails.root + "spec/support/rails.png")
+
+      click_on "Save as draft"
+
+      within "span.title" do
+        click_on "xColor rocks"
+      end
+
+      expect(page).to have_css "h5", text: "Uploads:"
     end
   end
 end
