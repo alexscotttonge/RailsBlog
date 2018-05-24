7d9ae5a8ac71ab2d80e95508cabd6d6a4929e946
alexscotttonge <alex@scotttonge.com>
2018-05-24 12:42:46 +0100

    Deleted feature spec and moved to separate spec file for attachments

--- spec/features/user_manages_blog_post_spec.rb
+++ spec/features/user_manages_blog_post_spec.rb
@@ -56,24 +56,6 @@ RSpec.feature "user creates a post" do
       expect(page).to have_content "xColor rocks"
     end
   end
-
-  context "when there is an attachment" do
-    it "the attachment is available on the posts#show view" do
-      log_user_in
-
-      fill_in "post_title", with: "xColor rocks"
-      fill_in "post_content", with: "Here are some reasons"
-      page.attach_file("post_uploads", Rails.root + "spec/support/rails.png")
-
-      click_on "Save as draft"
-
-      within "span.title" do
-        click_on "xColor rocks"
-      end
-
-      expect(page).to have_css "h5", text: "Uploads:"
-    end
-  end
 end
 
 RSpec.feature "editing a post" do
